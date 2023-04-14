0be1890fd0a78eb443c5981d5c6910fc0789ce6c
emrecoban <fbdarktole@gmail.com>
2023-04-14 23:09:27 +0300

    Added image-to-base64

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import fetch from "node-fetch";
 import cors from "cors";
+import imageToBase64 from "image-to-base64";
 
 const app = express();
 app.use(cors({
@@ -10,9 +11,25 @@ app.use(cors({
 app.get('/getEntry', async (req, res) => {
   try {
     const url = req.query.url;
-    const response = await fetch(url);
-    const html = await response.text();
-    res.send(html);
+    console.log("GELEN url => ", url.includes("ekstat"))
+    if(url.includes("ekstat")){
+      imageToBase64(url)
+      .then(
+          (response) => {
+              res.send(`data:image/jpeg;base64,${response}`);
+          }
+      )
+      .catch(
+          (error) => {
+              console.log(error);
+          }
+      )
+    }else{
+      const response = await fetch(url);
+      const html = await response.text();
+      res.send(html);
+    }
+    
   } catch (error) {
     console.error(error);
     res.status(500).send(error.message);
